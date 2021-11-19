b76563be94a4d19871109aef467efd18fe23b1de
mhamerlaine <mhamerlaine@synox.io>
2021-11-19 16:02:39 +0100

    Initial commit

---
+++ testApp/src/main/java/com/test/testApp/TestAppApplication.java
@@ -0,0 +1,18 @@
+package com.test.testApp;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAppApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestAppApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("init !!");
+	}
+}
